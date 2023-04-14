ac18c1aecbca54230b5b01b55e01a4cffb438611
j. Antonio Chavez <jesus.chavez@suramexico.com>
2023-04-14 13:08:23 -0600

    Initial Commit

---
+++ ExpenseTracker-SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ExpenseTracker-SwiftUI
+//
+//  Created by Antonio Chavez Saucedo on 14/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExpenseTracker-SwiftUI/ExpenseTracker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTracker_SwiftUIApp.swift
+//  ExpenseTracker-SwiftUI
+//
+//  Created by Antonio Chavez Saucedo on 14/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTracker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
